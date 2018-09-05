f23bcd308a667bf6ef8858c1ec299c29561e9d3b
Robert Ruiz <rsblooxpert@gmail.com>
2018-09-05 16:27:03 -0700

    Initial Commit

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+admin.initializeApp(functions.config().firebase);
+
+var db = admin.firestore();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.createPost = functions.https.onRequest((request,response) => {
+
+})
---
+++ public/index.js
@@ -0,0 +1,22 @@
+// Login Users:
+
+// Logout Users:
+
+// Firebase Auth:
+
+// Upload Project:
+function submitProject(e){
+
+}
+// Display Project:
+
+// Event Listeners:
+// Shortcuts to DOM Elements:
+let submitProjectElement = document.getElementById('submitProject');
+let signOutElement = document.getElementById('signOut');
+let signInElement = document.getElementById('signIn');
+
+// Saves message on form submit.
+submitProjectElement.addEventListener('submit', submitProject);
+signOutButtonElement.addEventListener('click', signOut);
+signInButtonElement.addEventListener('click', signIn);
